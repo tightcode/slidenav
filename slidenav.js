d15338b66ff2663e83d724b18ca55411d386f94b
tightcode <tightcodedev@gmail.com>
2019-05-21 19:44:50 -0700

    4.1.0-alpha.1 major update

---
+++ slidenav.js
@@ -0,0 +1,10 @@
+/* Slidenav 4.1.0-alpha.1 ( https://github.com/tightcode/slidenav ) */
+$(".dropdown").on("hide.bs.collapse", function() {
+  $(this).find(".dropdown-menu").first().addClass( "sliding" )
+});
+$(".dropdown").on("hidden.bs.collapse", function() {
+  $(this).find(".dropdown-menu").first().removeClass( "sliding" )
+});
+$(document).click(function() {
+  $(".dropdown-menu.collapse.show").collapse("hide");
+});
