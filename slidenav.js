168f352f2f973670feea16fbf532356f2e14e404
tightcode <tightcodedev@gmail.com>
2019-05-21 19:44:50 -0700

    4.1.0-alpha.1 major update

--- slidenav.js
+++ slidenav.js
@@ -1,44 +1,10 @@
-/* Slidenav 4.0.0-alpha ( https://github.com/tightcode/slidenav ) */
-
-// CSS media feature is used to detect if the user has requested that the system minimize the amount of animation or motion it uses
-var mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
-var slideSpeed = 350;
-
-var slideSpeedInit = function () {
-    if (mediaQuery.matches) {
-        slideSpeed = 1;
-    } else {
-        slideSpeed = 350;
-    }
-};
-
-window.addEventListener("load", slideSpeedInit);
-mediaQuery.addEventListener("change", slideSpeedInit);
-
-// Jquery slide effect
-$('.jq-slide').on('show.bs.dropdown', function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
+/* Slidenav 4.1.0-alpha.1 ( https://github.com/tightcode/slidenav ) */
+$(".dropdown").on("hide.bs.collapse", function() {
+  $(this).find(".dropdown-menu").first().addClass( "sliding" )
 });
-$('.jq-slide').on('hide.bs.dropdown', function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
+$(".dropdown").on("hidden.bs.collapse", function() {
+  $(this).find(".dropdown-menu").first().removeClass( "sliding" )
 });
-
-// JqueryUI slide effect
-$('.jqui-slide').on('show.bs.dropdown', function() {
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
-  } else {
-    $(this).find('.dropdown-menu').first().stop(true, true).show("slide", {
-      direction: 'up'
-    }, slideSpeed);
-  }
+$(document).click(function() {
+  $(".dropdown-menu.collapse.show").collapse("hide");
 });
-$('.jqui-slide').on('hide.bs.dropdown', function() {
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
-  } else {
-    $(this).find('.dropdown-menu').first().stop(true, true).hide("slide", {
-      direction: 'up'
-    }, slideSpeed);
-  }
-});	
